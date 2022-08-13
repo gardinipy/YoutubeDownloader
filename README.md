2aed058121afa1caba09de99fc49c4eff1dc9aef
fakhirpy <trjakira@gmail.com>
2022-08-13 12:54:10 -0300

    Version 1.0 Youtube Downloader

---
+++ README.md
@@ -0,0 +1,3 @@
+# YoutubeDownloader
+
+# python ytb.py
--- ytb.py
+++ ytb.py
@@ -1,9 +1,5 @@
 import pytube
 
-select = input("Select audio or video: ")
-
-url = input("Url video:")
-
 # to use in termux or linux and the directory is not "C:" change to the desired location.
 path= r'C:\Users\Administrator\Downloads\YTBDownloader'
 
@@ -13,12 +9,13 @@ def dvideo():
 def daudio():
     pytube.YouTube(url).streams.get_audio_only().download(path)
     
-if select == "Video" or "video":
+select = input("Select audio or video: ")
+
+url = input("Url video:")
+    
+if select == "Video" or select == "video":
     dvideo()
-if select == "Audio" or "audio":
+elif select == "Audio" or select == "audio":
     daudio()
-
-
-
-
-
+else:
+    print('Please select audio or video')
